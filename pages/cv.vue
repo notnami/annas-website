8ed0246e8316d90231144c189219347bdbc486e5
anna.melnikov <anna.me@hey.com>
2021-02-07 15:34:06 -0600

    Initial commit

---
+++ pages/cv.vue
@@ -0,0 +1,72 @@
+<template>
+
+<div class="container">
+<div class="resume">
+<h1>anna melnikov</h1>
+<span class="c4 c1">Machine Learning engineer with a
+focus on NLP and big data. Experienced with leading projects,
+organizing work across remote and distributed teams with diverse
+areas of expertise. Focused on delivering high-value products with
+clear metrics to reach concrete business goals for organizations
+and their clients.</span>
+
+<p class="c9"></p>
+<p class="c7"><span class="highlights">Highlights</span></p>
+
+<ul>
+  <li>Over 5 years of professional experience with Python</li>
+  <li>Machine Learning
+    <ul>
+      <li>Pytorch, Tensorflow, sklearn</li>
+    </ul></li>
+  <li>Data processing and analysis: PySpark, Pandas, Dask</li>
+  <li> Cloud
+    <ul>
+      <li>AWS: S3, EMR, Lambda, Athena, Sagemaker, CloudWatch</li>
+      <li>Google Cloud Platform: Kubernetes Engine, Container Registry, Google Storage, Firestore, Stackdriver</li>
+    </ul>  
+  </li>  
+  <li>Containers: Docker, Kubernetes</li>
+  <li>Data: MongoDB, Athena (PrestoDB), Hive, PostgreSQL</li>
+  <li>Web: Flask, async HTTP</li>
+</ul>
+
+      <div class="links">
+        <a href="/resume_anna.pdf"
+           target="_blank"
+         class="button--grey">full cv</a>
+        <a 
+           href="/"
+           class="button--grey">home</a>
+      </div>
+</div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
+  color: #000;
+  font-size: 20px;
+}
+
+
+.highlights {
+  font-style: bold;
+  color: #FF33B1;
+  font-size: 24px;
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        <span class="firstname">anna</span> <span class="middlename">hope</span>
+         <span class="lastname"> melnikov</span>
+      </h1>
+      <div class="links">
+        <NuxtLink to="/cv" class="button--grey">cv</NuxtLink>
+        <a
+          href="https://github.com/notnami"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          github
+        </a>
+        <a
+          href="https://www.linkedin.com/in/anna-melnikov/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          linkedin
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #e2e0e8;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 2px;
+}
+
+.firstname {
+  color: #55cdfc;
+}
+
+.middlename {
+  color: #fffdfb;
+}
+
+.lastname {
+  color: #f798b3;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+  
+}
+
+</style>
